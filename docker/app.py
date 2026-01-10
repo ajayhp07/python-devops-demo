3a5ee117af939efcd0eb84e669bd65fdec0527ff
AJAY PATEL <99001529+ajayhp07@users.noreply.github.com>
2026-01-10 07:56:17 +0530

    modification

--- docker/app.py
+++ docker/app.py
@@ -1,14 +1,32 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
+from prometheus_client import Counter, generate_latest
+import socket
+import datetime
 
 app = Flask(__name__)
 
+REQUEST_COUNT = Counter(
+    "http_requests_total",
+    "Total HTTP Requests",
+    ["method", "endpoint"]
+)
+
+@app.before_request
+def before_request():
+    REQUEST_COUNT.labels(method="GET", endpoint="/").inc()
+
 @app.route("/")
 def home():
-    return "Hello DevOps"
+    return render_template(
+        "index.html",
+        hostname=socket.gethostname(),
+        time=datetime.datetime.now()
+    )
 
 @app.route("/health")
 def health():
-    return jsonify(status="OK")
+    return jsonify(status="OK"), 200
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+@app.route("/metrics")
+def metrics():
+    return generate_latest(), 200
